891e84dce44487ff9b3263f2ff374514942c78c3
Denis Plaksin <dr.hy6yc@gmail.com>
2024-07-23 19:58:29 +0300

    Добавил .env для настроек redis

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
+from os import getenv
+from dotenv import load_dotenv
 import redis
 
-r = redis.Redis(host='localhost', port=6380, db=0, username='plakdi', password='30993054x@')
+load_dotenv()
+r = redis.Redis(
+    host=getenv('REDIS_HOST'),
+    port=getenv('REDIS_PORT'),
+    db=0, username=getenv('REDIS_USER'),
+    password=getenv('REDIS_USER_PASSWORD')
+)
 
 try:
     info = r.info()
     print(info['redis_version'])
     response = r.ping()
+
     if response:
+
         print("Подключение успешно!")
+        r.close()
+
     else:
         print("Не удалось подключиться к Redis.")
+        r.close()
+
+    r.set('user', 'plakdi')
+    print(r.ping())
+    print(type(r.get('user')))
 except redis.exceptions.RedisError as e:
     print(f"Ошибка: {e}")
+    r.close()
